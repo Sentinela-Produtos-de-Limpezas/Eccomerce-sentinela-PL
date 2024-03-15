87c43e9d4330c1ba43c35d6dba8a9e4fc8c1059e
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-03-15 13:32:19 -0300

    feat: :speech_balloon: adiciona texto no sistema

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -10,7 +10,25 @@ const users = [
 
 
 app.get("/", function (req: Request, res: Response) {
-  res.send("Bohr Express template");
+  res.send(`
+  Oi! Essa é a API do sistema Eccomerce da sentinela
+  Mas o que é uma API?
+
+Imagine que você está em um restaurante e deseja fazer um pedido. Você não entra na cozinha e mexe nas panelas, certo? Você pede ao garçom, que anota seu pedido e o leva para a cozinha. A API é como o garçom: ela recebe sua solicitação e a envia para a parte interna do sistema, que é o backend.
+
+E o que é o backend?
+
+O backend é como a cozinha do restaurante. É onde a "mágica" acontece, processando sua solicitação e preparando a resposta. No caso do Eccomerce da sentinela, o backend é responsável por:
+
+- Armazenar e gerenciar seus dados
+- Realizar cálculos e processamentos
+- Controlar o acesso ao sistema
+- Garantir a segurança das informações
+Em resumo:
+
+A API é a porta de entrada para o Eccomerce da sentinela.
+O backend é o "motor" que faz o sistema funcionar.
+  `);
 });
 
 app.get("/users", function (req: Request, res: Response) {
