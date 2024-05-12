d3a2e0ca014cc00ce957648b474a850cf8d713b0
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-05-12 14:24:52 -0300

    adiciona regra de negocios

--- api/core/src/service/Product.service.ts
+++ api/core/src/service/Product.service.ts
@@ -1,25 +1,56 @@
 import { Product } from '../model/Product.model';
+import { BaseError } from '../helpers/BaseError';
+import { StatusCode } from '../helpers/controllerStatusCode';
+import { productInput } from '../types/products/product';
+import { getObject } from '../helpers/FileBase';
+// import {} from ""
 
 export const ProductServices = {
   async getAll() {
     try {
-      return await Product.get()
+      const products = await Product.get()
+      if (products.isLeft()) return new BaseError(products.value.message, products.value.statusCode)
+      if (products.isRight()) return products.value
     } catch (error) {
-      return error
+      return new BaseError("error", StatusCode.INTERNAL_SERVER_ERROR)
     }
   },
   async getOne(id: number) {
     try {
-      return await Product.getOne(id)
+      const product = await Product.getOne(id)
+      if (product.isRight()) {
+        if (product.value === null) return new BaseError("Produto não encontrado !", StatusCode.NOT_FOUND)
+
+        return product.value
+      }
+      if (product.isLeft()) return new BaseError(product.value.message, StatusCode.INTERNAL_SERVER_ERROR)
     } catch (error) {
       return error
     }
   },
-  async create(productDate: any) {
+  async create(productDate: productInput, nameFile: string) {
     try {
-      return await Product.create(productDate)
+      const existProduct = await Product.findValidationCreateProduct(productDate.name)
+
+      const fileUrl = await getObject(nameFile)
+
+      if (existProduct.isRight()) {
+
+        const CreatedProduct = await Product.create({
+          ...productDate,
+          price: +productDate.price,
+          image: fileUrl as string
+        })
+        if (CreatedProduct.isLeft()) {
+          return new BaseError(CreatedProduct.value.message, CreatedProduct.value.statusCode)
+        }
+        if (CreatedProduct.isRight()) return CreatedProduct.value
+      }
+      if (existProduct.isLeft() && !existProduct.value) return new BaseError("Produto já cadastrado!", StatusCode.BAD_REQUEST)
+
+
     } catch (error) {
-      return error
+      return new BaseError("error", StatusCode.INTERNAL_SERVER_ERROR)
     }
   },
   async update(id: number, productDate: any) {
@@ -36,5 +67,5 @@ export const ProductServices = {
     } catch (error) {
       return error
     }
-  }
+  },
 }
