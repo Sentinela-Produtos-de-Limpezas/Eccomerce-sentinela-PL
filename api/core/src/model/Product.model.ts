f3e95754a1537a4336cd39781fd56123b41a05f5
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-05-12 14:31:49 -0300

    adiciona baseerror

--- api/core/src/model/Product.model.ts
+++ api/core/src/model/Product.model.ts
@@ -1,47 +1,71 @@
-import { productInput, productUpdate } from "@/types/products/product"
+import { productInput, productOutput, productUpdate } from "../types/products/product"
 import prisma from "./prisma"
+import { Either, left, right } from '@sweet-monads/either';
+import { BaseError } from '../helpers/BaseError';
 
 
 export const Product = {
-  async get() {
+  async get(): Promise<Either<BaseError, productOutput[]>> {
     try {
-      return await prisma.product.findMany()
+      const products = await prisma.product.findMany()
+      return right(products)
     } catch (error) {
-      return error
+      return left(new BaseError("Ocorreu um erro inesperado!"))
     }
   },
 
-  async getOne(id: number) {
-    try {
-      return await prisma.product.findUnique({ where: { id } })
-
-    } catch (error) {
-      return error
-    }
-  },
-
-  async create(productDate: productInput) {
-    try {
-      return await prisma.product.create({ data: productDate})
-    } catch (error) {
-      return error
-    }
-  },
-
-  async update(id: number, productDate: productUpdate) {
+  async getOne(id: number): Promise<Either<BaseError, productOutput | null>> {
     try {
       const product = await prisma.product.findUnique({ where: { id } })
-      return product
+      return right(product)
     } catch (error) {
-      return error
+      return left(new BaseError("Ocorreu um erro inesperado!"))
     }
   },
 
-  async delete(id: number) {
+  async create(productDate: productInput): Promise<Either<BaseError, productOutput>> {
     try {
-      return await prisma.product.delete({ where: { id } })
+      const newProduct = await prisma.product.create({ data: productDate })
+      return right(newProduct)
     } catch (error) {
-      return error
+      return left(new BaseError("Ocorreu um erro inesperado!"))
+    }
+  },
+
+  async update(id: number, productDate: productUpdate): Promise<Either<BaseError, productOutput>> {
+    try {
+      const product = await prisma.product.update({ where: { id }, data: productDate })
+
+      return right(product)
+    } catch (error) {
+      return left(new BaseError("Ocorreu um erro inesperado!"))
+    }
+  },
+
+  async delete(id: number): Promise<Either<BaseError, boolean>> {
+    try {
+      await prisma.product.delete({ where: { id } })
+      return right(true)
+    } catch (error) {
+      return left(new BaseError("Ocorreu um erro inesperado!"))
+    }
+  }, 
+
+  async findValidationCreateProduct(name: string, ): Promise<Either<false | BaseError, true>> {
+    try {
+      const existingProduct = await prisma.product.findFirst({
+        where: {
+          name
+        }
+      })
+
+      if(existingProduct) {
+        return left(false)
+      }
+
+      return right(true)
+    } catch (error) {
+      return left(new BaseError("Ocorreu um erro inesperado!"))
     }
   }
 
