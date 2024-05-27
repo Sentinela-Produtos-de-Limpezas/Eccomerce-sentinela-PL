bca8cc1820ff3959cb80fa068f7135c4172710c3
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-05-27 14:50:32 -0300

    adiciona o cors

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import routes from "./routes/index";
-
+import cors from "cors";
 
 const app = express();
+const options:cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "*",
+  preflightContinue: false
+};
+
+app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
