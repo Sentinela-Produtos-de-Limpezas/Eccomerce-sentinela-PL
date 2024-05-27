410bae9b4a576bb6b14bd681b657032bbf9b2096
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-05-27 14:49:16 -0300

    adiciona seeds

--- api/core/prisma/seed.ts
+++ api/core/prisma/seed.ts
@@ -1,98 +1,235 @@
 import prisma from '../src/model/prisma';
 import { hashPassword } from '../src/helpers/saltPassword';
+import { fakerPT_BR as faker } from '@faker-js/faker';
+import { userInput } from '../src/types/user/user';
+import { productInput } from '../src/types/products/product';
+
 
 async function seed() {
-  // Wrap all data creation within a transaction
+
+  const Categories = [
+    { name: "Eletrônicos", description: "Aparelhos eletrônicos" },
+    { name: "Roupas", description: "Itens de vestuário" },
+    { name: "Livros", description: "Literatura e leitura" },
+  ]
+
+  const Users: userInput[] = [
+    {
+      name: "Lucas",
+      lastname: "Bispo Menezes",
+      email: "teste@teste.com",
+      password: hashPassword("teste123456"),
+      phone: faker.phone.number(),
+      cpforcnpj: `${faker.number.int({ min: 10000000000, max: 99999999999 })}`,
+    },
+    {
+      name: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: hashPassword(faker.internet.password()),
+      phone: faker.phone.number(),
+      cpforcnpj: `${faker.number.int({ min: 10000000000, max: 99999999999 })}`,
+    },
+    {
+      name: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: hashPassword(faker.internet.password()),
+      phone: faker.phone.number(),
+      cpforcnpj: `${faker.number.int({ min: 10000000000, max: 99999999999 })}`,
+    },
+    {
+      name: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: hashPassword(faker.internet.password()),
+      phone: faker.phone.number(),
+      cpforcnpj: `${faker.number.int({ min: 10000000000, max: 99999999999 })}`,
+    }
+  ]
+
+  const produtos: productInput[] = [
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    },
+    {
+      name: faker.commerce.productName(),
+      price: +faker.commerce.price(),
+      image: `https://picsum.photos/seed/phone/500/700`,
+      sku: faker.commerce.isbn()
+    }
+  ]
+
+
   await prisma.$transaction(async (tx) => {
-    const user1 = await tx.user.create({
-      data: {
-        name: "John Doe",
-        lastname: "Smith",
-        email: "john.doe@example.com",
-        password: hashPassword("hashed_password"), // Replace with a secure password hashing mechanism
-        phone: "(123) 456-7890",
-        cpforcnpj: "12345678900001",
-        address: {
-          create: {
-            Street: "123 Main St",
-            Number: "10",
-            City: "Anytown",
-            isMain: true,
-          },
+
+
+    for (const category of Categories) {
+      await tx.category.create({
+        data: {
+          name: category.name,
+          description: category.description,
+        }
+      })
+    }
+
+    for (const user of Users) {
+      await tx.user.create({
+        data: {
+          name: user.name,
+          lastname: user.lastname,
+          email: user.email,
+          password: user.password,
+          phone: user.phone,
+          cpforcnpj: user.cpforcnpj,
+          address: {
+            create: {
+              Street: faker.location.street(),
+              Number: faker.location.buildingNumber(),
+              City: faker.location.city(),
+              isMain: true,
+            }
+          }
         },
-      },
-    });
+      })
+    }
 
-    const user2 = await tx.user.create({
-      data: {
-        name: "Jane Smith",
-        lastname: "Doe",
-        email: "jane.smith@example.com",
-        password: hashPassword("hashed_password"), // Replace with a secure password hashing mechanism
-        phone: "(987) 654-3210",
-        cpforcnpj: "98765432100002",
-        address: {
-          create: {
-            Street: "456 Elm St",
-            Number: "20",
-            City: "Big City",
-            isMain: true,
-          },
-        },
-      },
-    });
-
-    const category1 = await tx.category.create({
-      data: {
-        name: "Electronics",
-        description: "Electronic devices",
-      },
-    });
-
-    const category2 = await tx.category.create({
-      data: {
-        name: "Clothing",
-        description: "Clothing items",
-      },
-    });
-
-    const produtos = [
-      "Limpa Tudo",
-      "Sabão de Lavar Louça",
-      "Detergente para Roupa",
-      "Limpa Piso",
-      "Limpa Vidros",
-      "Desinfetante para Banheiro",
-      "Limpa Forno",
-      "Spray Desinfetante",
-      "Esponja",
-      "Escova de Limpeza",
-      "Mop",
-      "Sacos de Lixo",
-    ];
-
-
-    for (const produtoNome of produtos) {
-      const placeholderImageBase = "https://via.placeholder.com/150"; // URL base para imagem placeholder
-
+    for (const produto of produtos) {
       await tx.product.create({
         data: {
-          name: produtoNome,
-          price: Math.random() * 10 + 5, // Preço aleatório entre R$5 e R$15
-          image: placeholderImageBase,
-          sku: `${produtoNome.replace(/\s/g, "")}123`, // Gera SKU baseado no nome do produto
+          name: produto.name,
+          price: produto.price,
+          image: produto.image,
+          sku: produto.sku,
           categories: {
-            create: {
-              category: {
-                connect: { id: category1.id },
-              },
-            },
-          },
-        },
-      });
+            create: [
+              {
+                category: {
+                  connect: {
+                    id: [1, 2, 3.4][Math.floor(Math.random() * 3)],
+
+                  }
+                }
+              }
+            ]
+          }
+        }
+      })
     }
   })
 }
+
 
 seed()
   .catch((e) => {
