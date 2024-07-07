c93b70674ad5f3716971447e2ef93f0c3c77b11a
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-07-07 13:39:19 -0300

    adiciona chaves ao products

--- api/core/src/model/Product.model.ts
+++ api/core/src/model/Product.model.ts
@@ -9,16 +9,51 @@ import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 export const Product = {
   async get(): Promise<Either<BaseError, productOutput[]>> {
     try {
-      const products = await prisma.product.findMany()
+      const products = await prisma.product.findMany({
+        include: {
+          avaliations: {
+            select: {
+              id: true,
+              comment: true,
+              rating: true,
+              user: {
+                select: {
+                  name: true,
+                  lastname: true
+
+                }
+              }
+            },
+          },
+        }
+      })
       return right(products)
     } catch (error) {
+      console.log("🚀 ~ get ~ error:", error)
       return left(new BaseError("Ocorreu um erro inesperado!"))
     }
   },
 
   async getOne(id: number): Promise<Either<BaseError, productOutput | null>> {
     try {
-      const product = await prisma.product.findUnique({ where: { id } })
+      const product = await prisma.product.findUnique({
+        where: { id },
+        include: {
+          avaliations: {
+            select: {
+              id: true,
+              comment: true,
+              rating: true,
+              user: {
+                select: {
+                  name: true,
+                  lastname: true
+
+                }
+              }
+            }
+          }
+        }})
       return right(product)
     } catch (error) {
       return left(new BaseError("Ocorreu um erro inesperado!"))
@@ -39,8 +74,8 @@ export const Product = {
       const product = await prisma.product.update({ where: { id }, data: productDate })
       return right(product)
     } catch (error) {
-      if(error instanceof PrismaClientKnownRequestError) {
-        return  error.code === 'SQLITE_CONSTRAINT' ?  left(new BaseError("Não foi possível atualizar o produto. O SKU e o nome devem ser únicos. Verifique se o novo SKU e nome já estão cadastrados para outro produto.", StatusCode.NOT_FOUND)) : left(new BaseError("Não foi possível atualizar o Produto!", StatusCode.BAD_REQUEST))
+      if (error instanceof PrismaClientKnownRequestError) {
+        return error.code === 'SQLITE_CONSTRAINT' ? left(new BaseError("Não foi possível atualizar o produto. O SKU e o nome devem ser únicos. Verifique se o novo SKU e nome já estão cadastrados para outro produto.", StatusCode.NOT_FOUND)) : left(new BaseError("Não foi possível atualizar o Produto!", StatusCode.BAD_REQUEST))
       }
       return left(new BaseError("Ocorreu um erro inesperado!"))
     }
@@ -48,32 +83,33 @@ export const Product = {
 
   async delete(id: number): Promise<Either<BaseError, boolean>> {
     try {
-       await prisma.product.delete({ 
-        where: {id},
-       }
-       )
+      await prisma.product.delete({
+        where: { id },
+      }
+      )
       return right(true)
     } catch (error) {
-      if(error instanceof PrismaClientKnownRequestError) {
-      return  error.code === 'P2025' ?  left(new BaseError("Produto não encontrado!", StatusCode.NOT_FOUND)) : left(new BaseError("Não foi possível deletar o Produto!", StatusCode.BAD_REQUEST))
-  
+      if (error instanceof PrismaClientKnownRequestError) {
+        return error.code === 'P2025' ? left(new BaseError("Produto não encontrado!", StatusCode.NOT_FOUND)) : left(new BaseError("Não foi possível deletar o Produto!", StatusCode.BAD_REQUEST))
+
       }
-      return left(new BaseError("Não foi possível deletar o Produto!", StatusCode.BAD_REQUEST))    }
-  }, 
+      return left(new BaseError("Não foi possível deletar o Produto!", StatusCode.BAD_REQUEST))
+    }
+  },
 
 
-  async findValidationCreateProduct(scope:ScopeValidationProduct ): Promise<Either<false | BaseError, true>> {
+  async findValidationCreateProduct(scope: ScopeValidationProduct): Promise<Either<false | BaseError, true>> {
     try {
       const existingProduct = await prisma.product.findFirst({
         where: {
           OR: [
-            {name: scope.name},
-            {sku: scope.sku},
+            { name: scope.name },
+            { sku: scope.sku },
           ]
         }
       })
 
-      if(existingProduct) {
+      if (existingProduct) {
         return left(false)
       }
 
