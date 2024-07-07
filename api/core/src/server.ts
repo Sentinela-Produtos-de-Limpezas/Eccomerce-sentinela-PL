7b5c29d843bab793c60e3d24ae012e3d328bb8ac
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-07-07 13:38:33 -0300

    corrige erro "cors quando tem uma credencial o oringin não pode ser *"

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -5,11 +5,8 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 const options:cors.CorsOptions = {
-  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
   credentials: true,
-  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: "*",
-  preflightContinue: false
+  origin: "http://localhost:5173",
 };
 
 app.use(cookieParser());
