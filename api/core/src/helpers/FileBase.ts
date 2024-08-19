2f3336fc36acc53c7bf7e5a650e5a5e2e833de3b
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-08-19 14:13:47 -0300

    renomeia variáveis de ambiente

--- api/core/src/helpers/FileBase.ts
+++ api/core/src/helpers/FileBase.ts
@@ -7,7 +7,7 @@ export const S3 = new S3Client({
   region: "us-east-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
+    secretAccessKey: process.env.FILE_STORAGE_AWS_SECRET_ACCESS_KEY as string
   }
 })
 
