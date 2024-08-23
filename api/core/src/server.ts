ad1892ac146df3f99a5a487489b96ffff4e90ef7
Lucas Bispo Menezes <lucasbispomenezes@hotmail.com>
2024-08-23 14:17:13 -0300

    update code

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -4,46 +4,23 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
-// Configuração das opções de CORS
-
-const allowedDomains = [
-  "https://devfrontend.bohr.io",
-]
-const corsOptions: cors.CorsOptions = {
+const options: cors.CorsOptions = {
   credentials: true,
-  origin:  function (origin, callback) {
-    // bypass the requests with no origin (like curl requests, mobile apps, etc )
-    if (!origin) return callback(null, true);
- 
-    if (allowedDomains.indexOf(origin) === -1) {
-      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}
+  origin: "https://devfrontend.bohr.io"
+};
 
-// Middleware para CORS
-app.use(cors(corsOptions));
+app.use(cors(options));
 
-// Middleware para parsing de cookies
+
 app.use(cookieParser());
-
-// Middleware para parsing de JSON
+app.use(cors(options));
 app.use(express.json());
-
-// Middleware para parsing de URLs codificadas
 app.use(express.urlencoded({ extended: true }));
-
-// Definindo as rotas
 app.use(routes);
 
-// Iniciando o servidor
 if (!module.parent) {
-  app.listen(3000, () => {
-    console.log("Express started on port 3000");
-  });
+  app.listen(3000);
+  console.log("Express started on port 3000");
 }
 
 export default app;
